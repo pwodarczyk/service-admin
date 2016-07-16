0b5cae400411efa55e42ab0d95f94b2aa7c3558e
pwodarczyk <philip.wodarczyk@gmail.com>
2016-07-16 01:10:21 -0500

    Add project files.

---
+++ ServiceAdmin/Controllers/DashboardController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using ServiceAdmin.Models;
+
+namespace ServiceAdmin.Controllers
+{
+    [Authorize]
+    public class DashboardController : Controller
+    {
+        private ApplicationSignInManager _signInManager;
+        private ApplicationUserManager _userManager;
+
+        public DashboardController()
+        {
+        }
+
+        public DashboardController(ApplicationUserManager userManager, ApplicationSignInManager signInManager)
+        {
+            UserManager = userManager;
+            SignInManager = signInManager;
+        }
+
+        public ApplicationSignInManager SignInManager
+        {
+            get
+            {
+                return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
+            }
+            private set 
+            { 
+                _signInManager = value; 
+            }
+        }
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+
+        //
+        // GET: /Dashboard/Index
+        public async Task<ActionResult> Index(string message)
+        {
+           
+            var userId = User.Identity.GetUserId();
+           
+            return View();
+        }
+
+    }
+}
---
+++ ServiceAdmin/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ServiceAdmin.MvcApplication" Language="C#" %>
